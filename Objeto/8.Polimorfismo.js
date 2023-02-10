b388a5059d03eced41db2ec99f989980401827b2
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-02-10 18:27:48 -0300

    fim da sessaõ

--- Objeto/8.Polimorfismo.js
+++ Objeto/8.Polimorfismo.js
@@ -5,20 +5,63 @@ function Conta(agencia, conta, saldo) {
     this.saldo = saldo;
 }
 
-Conta.prototype.sacar = (valor) => {
+Conta.prototype.sacar = function (valor){
+    if(this.saldo < valor){
+        console.log('Saldo insuficiente:'+ `${this.saldo}`)
+        this.verSaldo();
+        return;
+    }
+
     this.saldo -= valor;
     this.verSaldo();
-}
+};
 
-Conta.prototype.depositar = (valor) => {
+Conta.prototype.depositar = function(valor) {
     this.saldo += valor;
     this.verSaldo();
-} 
+};
 
-Conta.prototype.verSaldo = () => {
+Conta.prototype.verSaldo = function() {
     console.log(
         `Ag/c:${this.agencia}/${this.conta} |` + `Saldo: R$${this.saldo.toFixed(2)}`
     )
-}
+};
 
 const conta1 = new Conta(11, 22, 10)
+conta1.depositar(11)
+
+function ContaCorrente(agencia, conta, saldo, limite) {
+    Conta.call(this, agencia, conta, saldo);
+    this.limite = limite;
+}
+
+ContaCorrente.prototype = Object.create(Conta.prototype);
+ContaCorrente.prototype.constructor = ContaCorrente;
+
+ContaCorrente.prototype.sacar = function (valor){
+    if(valor > (this.saldo + this.limite)){
+        console.log('Saldo insuficiente:'+ `${this.saldo}`)
+        this.verSaldo();
+        return;
+    }
+    
+    this.saldo -= valor;
+    this.verSaldo();
+};
+
+const contaCorrente = new ContaCorrente(14, 22, 0, 100);
+contaCorrente.depositar(10)
+contaCorrente.sacar(110)
+contaCorrente.sacar(2)
+
+function ContaPoupanca(agencia, conta, saldo) {
+    Conta.call(this, agencia, conta, saldo);
+}
+
+ContaPoupanca.prototype = Object.create(Conta.prototype);
+ContaPoupanca.prototype.constructor = ContaPoupanca;
+
+const contaPoupanca = new ContaPoupanca(14, 22, 0,);
+contaPoupanca.depositar(10)
+contaPoupanca.sacar(110)
+contaPoupanca.sacar(2)
---
+++ Objeto/Map.js
@@ -0,0 +1,5 @@
+const pessoas = [
+    { id: 3, nome:'Noah'},
+    { id: 2, nome:'Lewis'},
+    { id: 1, nome:'Helen'}
+];
