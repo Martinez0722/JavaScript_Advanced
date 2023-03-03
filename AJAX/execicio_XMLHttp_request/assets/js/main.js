ece514202ac26de48b04c6ccb411a4e34ad7e4b7
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-03-03 11:27:14 -0300

    fetcxh e xml

--- AJAX/execicio_XMLHttp_request/assets/js/main.js
+++ AJAX/execicio_XMLHttp_request/assets/js/main.js
@@ -1,19 +1,51 @@
-const request = obj => {
-    const xhr = new XMLHttpRequest();
-    xhr.open(obj.method, obj.url, true);
-    xhr.send();
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
+    
+//         xhr.addEventListener('load', () => {
+//             if(xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText)
+//             } else {
+//                 reject(xhr.statusText
+//                 )
+//             }
+//         })
+//     })
+// };
 
-    xhr.addEventListener('load', () => {
-        if(xhr.status >= 200 && xhr.status < 300) {
-            obj.success(xhr.responseText)
-        } else {
-            obj.error({
-                code: xhr.status,
-                msg: xhr.statusText
-            })
-        }
-    })
-};
+// document.addEventListener('click', e => {
+//     const el = e.target;
+//     const tag = el.tagName.toLowerCase();
+
+//     if(tag === 'a') {
+//         e.preventDefault();
+//       carregaPagina(el)  
+//     }
+// })
+
+// async function carregaPagina(el) {
+//     const href = el.getAttribute('href');
+    
+//     const objConfig = {
+//         method: 'GET',
+//         url: href
+//     };
+//     try {
+//         const response = await request(objConfig);
+//         carregaResultado(response);
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
+// function carregaResultado ( response) {
+//     const resultado = document.querySelector('.resultado');
+//     resultado.innerHTML = response;
+// }
+
+// fetch api
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -25,22 +57,23 @@ document.addEventListener('click', e => {
     }
 })
 
-function carregaPagina(el) {
-    const href = el.getAttribute('href');
-    
-    request({
-        method: 'GET',
-        url: href,
-        success(response) {
-            carregaResultado(response)
-        },
-        error (errorText) {
-            console.log(errorText)
-        }
-    });
-}
 
-function carregaResultado ( response) {
+async function carregaPagina(el) {
+    try {
+        const href = el.getAttribute('href');
+        const res = await fetch(href);
+        
+        if(res.status !== 200) throw new Error('ERRO 404!');
+        
+        const html = await res.text();
+        carregaResultado(html);
+    } catch (e) {
+        console.error(e)
+    }
+}
+    
+
+function carregaResultado (res) {
     const resultado = document.querySelector('.resultado');
-    resultado.innerHTML = response;
+    resultado.innerHTML = res;
 }
