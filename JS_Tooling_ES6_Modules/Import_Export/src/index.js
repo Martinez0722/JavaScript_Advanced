1dd81398c265a92dd75b690d43f291e8014832d4
Rafael Felipe <102329554+Martinez0722@users.noreply.github.com>
2023-03-04 21:37:41 -0300

    import/export

---
+++ JS_Tooling_ES6_Modules/Import_Export/src/index.js
@@ -0,0 +1,11 @@
+// import { nome as nome2, sobrenome, idade, soma, salario } from './modulo1';
+
+import * as meuModulo from './modulo1';
+
+console.log(meuModulo);
+
+// const nome = 'Gael'
+
+// console.log({nome ,nome2, sobrenome, idade, salario});
+
+// console.log(soma(5, 10))
---
+++ JS_Tooling_ES6_Modules/Import_Export/src/modulo1.js
@@ -0,0 +1,10 @@
+export const nome = 'Graham';
+export const sobrenome = 'Sarri';
+export const idade = 30;
+export const salario = 5000;
+const cpf = '2101100'
+
+export function soma(x, y) {
+    return x + y;
+};
+
